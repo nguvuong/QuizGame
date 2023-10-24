6f4936c25ef228c723f5a4d361dfd09cfa810fc7
Vuong Quoc Nguyen <nguvuong@sheridancollege.ca>
2023-10-24 16:00:10 -0400

    finish building splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.nguvuong.quizgame"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.nguvuong.quizgame"
@@ -15,6 +15,11 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+//    use android view binding
+    buildFeatures{
+        viewBinding = true
     }
 
     buildTypes {
